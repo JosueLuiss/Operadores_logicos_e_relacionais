7b33e442b400bf3bcd0a82defc78f49720ba436a
JosueLuiss <santossjosue21@gmail.com>
2025-07-10 12:10:14 -0300

    Update Operadores_lógicos_e_relacionais.py

--- Operadores_lógicos_e_relacionais.py
+++ Operadores_lógicos_e_relacionais.py
@@ -1,40 +1,39 @@
-"""
-n1 = int(input("Informe um número:"))
-n2 = int(input("Informe o segundo número: "))
 
-soma = n1 + n2
-media = (n1 + n2) / 2
+# n1 = int(input("Informe um número:"))
+# n2 = int(input("Informe o segundo número: "))
 
-print("A soma entre {} e {} é {}" .format(n1, n2, soma))
-print("A média entre {} e {} é {}" .format(n1, n2, media))
-"""
+# soma = n1 + n2
+# media = (n1 + n2) / 2
+
+# print("A soma entre {} e {} é {}" .format(n1, n2, soma))
+# print("A média entre {} e {} é {}" .format(n1, n2, media))
 
 
-"""
-A = 1
-B = 2
-C = 3
 
-print(A == B)
-print((A + B) == C)
-print((C - B) == A)
-print((A ^ 2) != B) 
-print(B >  (C - 1))
-print((A + B == C) or (C % B == A))
-print((C - B <= A) and (A + C > B))
-"""
 
-"""
-n1 = float(input("Informe o primeiro lado do triangulo: "))
-n2 = float(input("Informe o segudo lado do triangulo: "))
-n3 = float(input("Informe o terceiro lado do triangulo: "))
+# A = 1
+# B = 2
+# C = 3
 
-eq = (n1 == n2) and (n2 == n3)
-es = (n1 != n2) and (n2 != n3) and (n1 != n3)
+# print(A == B)
+# print((A + B) == C)
+# print((C - B) == A)
+# print((A ^ 2) != B) 
+# print(B >  (C - 1))
+# print((A + B == C) or (C % B == A))
+# print((C - B <= A) and (A + C > B))
 
-tri = n1 < (n2 + n3) and n2 < (n1 + n3) and n3 < (n1 + n2)
 
-print("O triangulo é equilatero? ", eq )
-print("O triangulo é escaleno? ", es)
-print("Posso formar um triangulo? ", tri)
-"""
+
+# n1 = float(input("Informe o primeiro lado do triangulo: "))
+# n2 = float(input("Informe o segudo lado do triangulo: "))
+# n3 = float(input("Informe o terceiro lado do triangulo: "))
+
+# eq = (n1 == n2) and (n2 == n3)
+# es = (n1 != n2) and (n2 != n3) and (n1 != n3)
+
+# tri = n1 < (n2 + n3) and n2 < (n1 + n3) and n3 < (n1 + n2)
+
+# print("O triangulo é equilatero? ", eq )
+# print("O triangulo é escaleno? ", es)
+# print("Posso formar um triangulo? ", tri)
